7512a9cc7b39ed1f1c09e991e71e688aaa2764e3
FredLoney <loneyf@ohsu.edu>
2014-12-17 12:24:32 -0800

    Import file and logging into the package.

--- qiutil/__init__.py
+++ qiutil/__init__.py
@@ -11,3 +11,15 @@ is incremented when there is a backward-compatible refactoring
 or bug fix. All major, minor and patch version numbers begin at
 1.
 """
+
+# Import file and logging, since these are also standard Python
+# libraries. This import allows the client to use the nested
+# modules directly, e.g.:
+#   with qiutil.file.open(...):
+# rather than:
+#   from qiutil import file
+#   with file.open(...): # Misleading
+#  or:
+#   from qiutil import file as qifile
+#   with qifile.open(...): # Awkward
+from . import (file, logging) 
