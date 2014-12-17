fd5ec3c9b01055485a0b7472666ff834f257d4f9
FredLoney <loneyf@ohsu.edu>
2014-12-17 12:31:00 -0800

    Test non-gz file open.

--- test/unit/test_file.py
+++ test/unit/test_file.py
@@ -5,17 +5,24 @@ from ..helpers.logging import logger
 import qiutil
 from test import ROOT
 
-FIXTURE = os.path.join(ROOT, 'fixtures', 'file', 'simple.txt.gz')
+FIXTURES = os.path.join(ROOT, 'fixtures', 'file')
 """The test fixture configuration file."""
 
 
 class TestFile(object):
     """file unit tests."""
 
+    def test_open_non_gz(self):
+        in_file = os.path.join(FIXTURES, 'simple.txt')
+        with qiutil.file.open(in_file) as fp:
+            line_cnt = len(fp.readlines())
+        assert_equal(line_cnt, 2, "%s line count is incorrect: %d" % (in_file, line_cnt))
+
     def test_open_gz(self):
-        with qiutil.file.open(FIXTURE) as fp:
+      in_file = os.path.join(FIXTURES, 'simple.txt.gz')
+      with qiutil.file.open(in_file) as fp:
           line_cnt = len(fp.readlines())
-        assert_equal(line_cnt, 2, "%s line count is incorrect: %d" % (FIXTURE, line_cnt))
+          assert_equal(line_cnt, 2, "%s line count is incorrect: %d" % (in_file, line_cnt))
 
 
 if __name__ == "__main__":
