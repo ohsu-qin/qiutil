febac7abef57515b14c28c2d33378c49c441b983
FredLoney <loneyf@ohsu.edu>
2014-12-17 12:25:40 -0800

    Don't test the hidden function.

--- test/unit/test_which.py
+++ test/unit/test_which.py
@@ -1,23 +1,19 @@
 import os
 import glob
-from nose.tools import (assert_is_none, assert_is_not_none, assert_true,
-                        assert_false)
-from ..helpers.logging import logger
+from nose.tools import (assert_is_none, assert_is_not_none)
 from qiutil import which
 
 
 class TestWhich(object):
-
     """which unit tests."""
 
     def test_which(self):
         python_path = which.which('python')
-        assert_is_not_none(python_path,
-                           'python is not discovered in the executable path')
-        assert_true(which.is_executable(python_path), 'python is not considered executable')
+        assert_is_not_none(python_path, 'python is not discovered in the'
+                                        ' executable path')
         bogus_path = which.which('bogus')
-        assert_is_none(bogus_path, "bogus is found in the executable path at: %s"
-                                   % bogus_path)
+        assert_is_none(bogus_path, "bogus is found in the executable path"
+                                   " at: %s" % bogus_path)
 
 
 if __name__ == "__main__":
