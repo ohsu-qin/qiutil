1b69f6f4e777e89dcdfa52af1a25def088e0800e
FredLoney <loneyf@ohsu.edu>
2015-01-08 18:25:05 -0800

    Test logging config file.

--- test/unit/test_logging.py
+++ test/unit/test_logging.py
@@ -1,24 +1,22 @@
 import os
 import shutil
-from nose.tools import (assert_equal, assert_true)
+from nose.tools import (assert_equal, assert_not_equal, assert_true)
 from qiutil import logging
 from qiutil.logging import logger
-
-ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
-"""The test parent directory."""
+from qiutil.cd import cd
+from .. import ROOT
 
 FIXTURE = os.path.join(ROOT, 'fixtures', 'logging', 'logging.yaml')
 """The test fixture logging configuration file."""
 
-RESULTS = os.path.join(ROOT, 'results', 'fixtures', 'logging')
+RESULTS = os.path.join(ROOT, 'results', 'logging')
 """The test result parent directory."""
 
-RESULT = os.path.join(RESULTS, 'log', 'qiutil.log')
+RESULT = os.path.join(RESULTS, 'log', 'test.log')
 """The resulting test log."""
 
 
-class TestLoggingHelper(object):
-
+class TestLogging(object):
     """The logging unit tests."""
 
     def setUp(self):
@@ -36,8 +34,8 @@ class TestLoggingHelper(object):
         with open(RESULT) as fs:
             msgs = fs.readlines()
         assert_true(not not msgs, "No log messages in %s" % RESULT)
-        assert_equal(len(msgs), 1, "Extraneous log messages in %s" % RESULT)
-
+        assert_equal(len(msgs), 1, "Extraneous log messages: %s" % msgs)
+    
     def test_level(self):
         logging.configure('test', filename=RESULT, level='DEBUG')
         logger('test').info("Test info log message.")
@@ -47,7 +45,24 @@ class TestLoggingHelper(object):
         with open(RESULT) as fs:
             msgs = fs.readlines()
         assert_true(not not msgs, "No log messages in %s" % RESULT)
+        assert_not_equal(len(msgs), 1, "Missing log messages in %s" % RESULT)
         assert_equal(len(msgs), 2, "Extraneous log messages in %s" % RESULT)
+
+    def test_config_file(self):
+        os.makedirs(RESULTS)
+        # Since the config fixture specifies a relative log file 'log/test.log',
+        # run this test in the context of the results directory.
+        with cd(RESULTS):
+            logging.configure('test', FIXTURE)
+            logger('test').info("Test info log message.")
+        assert_true(os.path.exists(RESULT), "The log file was not created: %s" %
+                                            RESULT)
+        with open(RESULT) as fs:
+          msgs = fs.readlines()
+          assert_true(not not msgs, "No log messages in %s" % RESULT)
+          msg = msgs[0]
+          assert_true(msg.startswith('Custom: '), "The log message format is"
+                                                  " incorrect: %s" % msg)
 
 if __name__ == "__main__":
     import nose
