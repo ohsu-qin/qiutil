9682aa9bf3e6782a4d5d35a9ef93014b60f05bb1
FredLoney <loneyf@ohsu.edu>
2015-01-08 17:47:51 -0800

    Make qiutil.collections accessible.

--- qiutil/__init__.py
+++ qiutil/__init__.py
@@ -12,9 +12,9 @@ or bug fix. All major, minor and patch version numbers begin at
 1.
 """
 
-# Import file and logging, since these are also standard Python
-# libraries. This import allows the client to use the nested
-# modules directly, e.g.:
+# Import collections, file and logging, since these are also
+# standard Python libraries. This import allows the client to
+# use the nested modules directly, e.g.:
 #   with qiutil.file.open(...):
 # rather than:
 #   from qiutil import file
@@ -22,4 +22,4 @@ or bug fix. All major, minor and patch version numbers begin at
 #  or:
 #   from qiutil import file as qifile
 #   with qifile.open(...): # Awkward
-from . import (file, logging) 
+from . import (collections, file, logging) 
