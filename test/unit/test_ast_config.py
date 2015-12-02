680d538cd40af3458a2f15a0323d6ec51a33cf4e
FredLoney <loneyf@ohsu.edu>
2015-12-02 11:50:42 -0800

    Test config write.

--- test/unit/test_ast_config.py
+++ test/unit/test_ast_config.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from nose.tools import (assert_equal, assert_is_not_none)
 from ..helpers.logging import logger
 from qiutil.ast_config import read_config
@@ -31,6 +32,38 @@ class TestASTConfig(object):
 
         assert_equal(opts, expected, "The configuration Tuning options are"
                      " incorrect: %s" % opts)
+
+    def test_write(self):
+        # The test config.
+        expected_cfg = read_config(FIXTURE)
+        with tempfile.NamedTemporaryFile() as copy:
+            # Write out a copy.
+            expected_cfg.write(copy)
+            copy.flush()
+            # Read the copy back in.
+            actual_cfg = read_config(copy.name)
+            # Compare the round-trip result.
+            expected_sections = set(expected_cfg.sections())
+            actual_sections = set(actual_cfg.sections())
+            assert_equal(actual_sections, expected_sections,
+                         "The configuration round-trip sections differ:"
+                         " %s vs %s" % (actual_sections, expected_sections))
+            for section in expected_sections:
+                expected_content = dict(expected_cfg.items(section))
+                actual_content = dict(actual_cfg.items(section))
+                expected_keys = set(expected_content.keys())
+                actual_keys = set(actual_content.keys())
+                
+                assert_equal(actual_keys, expected_keys,
+                             "The configuration round-trip %s section keys"
+                             " differ: %s vs %s" %
+                             (section, actual_keys, expected_keys))
+                for key, expected_value in expected_content.items():
+                    actual_value = actual_content[key]
+                    assert_equal(actual_value, expected_value,
+                                 "The configuration round-trip %s section %s"
+                                 " values differ: %s vs %s" %
+                                 (section, key, actual_value, expected_value))
 
 
 if __name__ == "__main__":
